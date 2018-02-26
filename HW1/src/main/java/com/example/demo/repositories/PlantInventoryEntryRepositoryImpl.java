75af35a5a00a8fa29cf91f556b958e83b8d23ca1
Ardi Aasmaa <aasmaa.ardi@gmail.com>
2018-02-26 20:11:14 +0200

    plantsAvailableForABusinessPeriod

--- HW1/src/main/java/com/example/demo/repositories/PlantInventoryEntryRepositoryImpl.java
+++ HW1/src/main/java/com/example/demo/repositories/PlantInventoryEntryRepositoryImpl.java
@@ -3,11 +3,12 @@ package com.example.demo.repositories;
 import com.example.demo.models.*;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.time.LocalDate;
 import java.util.List;
+
+import static com.example.demo.models.EquipmentCondition.SERVICEABLE;
 
 @Repository
 @Transactional(readOnly = true)
@@ -24,11 +25,11 @@ public class PlantInventoryEntryRepositoryImpl implements PlantSearchRepository 
 	@Override
 	public List<PlantsWithCount> findAvailable(String name, LocalDate start, LocalDate end) {
 		return entityManager.createQuery("select new com.example.demo.models.PlantsWithCount(pii.plantInfo, count(pii)) " +
-						"from PlantInventoryItem pii where " +
+						"from PlantInventoryItem pii where pii.equipmentCondition = ?4 and " +
 						"lower(pii.plantInfo.name) like lower(concat('%', ?1,'%')) " +
 						"and pii.id not in (select pr.plant.id from PlantReservation pr where " +
 						"pr.schedule.startDate <= ?3 and pr.schedule.endDate >= ?2) group by pii.plantInfo",
-				PlantsWithCount.class).setParameter(1,name).setParameter(2, start).setParameter(3, end).getResultList();
+				PlantsWithCount.class).setParameter(1, name).setParameter(2, start).setParameter(3, end).setParameter(4, SERVICEABLE).getResultList();
 	}
 
 }
