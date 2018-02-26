afb1788b3a72ece472187cb3beb2177232f51f59
Ardi Aasmaa <aasmaa.ardi@gmail.com>
2018-02-26 22:55:17 +0200

    isThereAnyAvailableItem

--- HW1/src/main/java/com/example/demo/repositories/PlantInventoryEntryRepositoryImpl.java
+++ HW1/src/main/java/com/example/demo/repositories/PlantInventoryEntryRepositoryImpl.java
@@ -19,17 +19,24 @@ public class PlantInventoryEntryRepositoryImpl implements PlantSearchRepository 
 
 	@Override
 	public boolean isThereAnyAvailableItem(PlantInventoryEntry entry, LocalDate start, LocalDate end) {
-		return false;
+		return entityManager.createQuery("select case when (count(pii) > 0) then true else false end from " +
+				"PlantInventoryItem  pii where pii.plantInfo = ?1 and pii.equipmentCondition = ?2 and " +
+				"pii.id not in (select pr.plant.id from PlantReservation pr where " +
+				"pr.schedule.startDate <= ?4 and pr.schedule.endDate >= ?3)", Boolean.class)
+				.setParameter(1, entry).setParameter(2, SERVICEABLE).setParameter(3, start)
+				.setParameter(4, end).getSingleResult();
 	}
 
 	@Override
 	public List<PlantsWithCount> findAvailable(String name, LocalDate start, LocalDate end) {
-		return entityManager.createQuery("select new com.example.demo.models.PlantsWithCount(pii.plantInfo, count(pii)) " +
+		return entityManager.createQuery("select new " +
+						"com.example.demo.models.PlantsWithCount(pii.plantInfo, count(pii)) " +
 						"from PlantInventoryItem pii where pii.equipmentCondition = ?4 and " +
 						"lower(pii.plantInfo.name) like lower(concat('%', ?1,'%')) " +
 						"and pii.id not in (select pr.plant.id from PlantReservation pr where " +
 						"pr.schedule.startDate <= ?3 and pr.schedule.endDate >= ?2) group by pii.plantInfo",
-				PlantsWithCount.class).setParameter(1, name).setParameter(2, start).setParameter(3, end).setParameter(4, SERVICEABLE).getResultList();
+				PlantsWithCount.class).setParameter(1, name).setParameter(2, start).setParameter(3, end)
+				.setParameter(4, SERVICEABLE).getResultList();
 	}
 
 }
