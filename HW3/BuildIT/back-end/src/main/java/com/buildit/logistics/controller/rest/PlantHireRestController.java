351cb072186e9fb990f9c3650e95d3336ca25e58
Polad Mahmudov <cool.polad@gmail.com>
2018-04-25 18:22:40 +0300

    Created file with state model and sequence diagram

--- HW3/BuildIT/back-end/src/main/java/com/buildit/logistics/controller/rest/PlantHireRestController.java
+++ HW3/BuildIT/back-end/src/main/java/com/buildit/logistics/controller/rest/PlantHireRestController.java
@@ -5,6 +5,7 @@ import com.buildit.logistics.application.services.PlantHireServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,32 +16,32 @@ public class PlantHireRestController {
     PlantHireServices plantHireServices;
 
     @PostMapping("/requests")
-    public Resource<?> createHireRequest(@RequestBody PlantHireRequestDTO request){
+    public ResponseEntity<?> createHireRequest(@RequestBody PlantHireRequestDTO request){
 //        TODO: create new plant hire request
         return null;
     }
 
     @GetMapping("/requests")
-    public Resources<?> getAllRequests(){
+    public ResponseEntity<?> findAllRequests(){
 //        TODO: return all plant hire requests
         return null;
     }
 
     @GetMapping("/requests/{id}")
-    public Resource<?> getRequest(@PathVariable(name = "id") Long requestId){
+    public ResponseEntity<?> findRequest(@PathVariable(name = "id") Long requestId){
 //        TODO: return plant hire requests with given id
         return null;
     }
 
     @PatchMapping("/request-management/requests/{id}")
-    public Resource<?> approveHireRequest(@PathVariable(name = "id") Long id,
+    public ResponseEntity<?> approveHireRequest(@PathVariable(name = "id") Long id,
                                           @RequestBody PlantHireRequestDTO updatedRequest){
 //        TODO: update the state of the plant hire request to ACCEPTED, generate and send new PO to RentIT
         return null;
     }
 
     @DeleteMapping("/request-management/requests/{id}")
-    public Resource<?> rejectHireRequest(@PathVariable(name = "id") Long id){
+    public ResponseEntity<?> rejectHireRequest(@PathVariable(name = "id") Long id){
 //        TODO: update the state of the plant hire request to REJECTED
         return null;
     }
