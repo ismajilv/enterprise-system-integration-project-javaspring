438b90cca8d0abf2bf97c82533fa53639f373467
John Doe <kadri.oluwagbemi@gmail.com>
2018-05-03 11:39:57 +0300

    For RentIT Test

--- HW3/RentIT/front-end/RentITfront/src/App.vue
+++ HW3/RentIT/front-end/RentITfront/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
     <h2> RentIT </h2>
-  <table class="table is-table-bordered is-table-striped is-fullwidth">
+    <h2> Lists of purchase Order</h2>
+    <table class="table is-table-bordered is-table-striped is-fullwidth">
     <thead>
-        <tr class="table-row-rentit">
+        <tr>
             <th class="has-text-center">id</th>
             <th class="has-text-center">Name</th>
             <th class="has-text-center">Start Date</th>
@@ -15,14 +16,66 @@
         </tr>
     </thead>
     <tbody>
+          <tr class="table-row-we2" v-for="pending in allrequest" :key="pending._id" >
+            <td class="has-text-center" id="name"> {{pending._id}}</td>
+            <td id = "plantNameWE2" class="has-text-center"> {{pending.plant.name}}</td>
+            <td id = "plantStartDateWE2" class="has-text-center"> {{pending.rentalPeriod.startDate}}</td>
+            <td id = "plantEndDateWE2" class="has-text-center"> {{pending.rentalPeriod.endDate}}</td>
+            <td class="has-text-center"> {{pending.requestingSiteEngineer.firstName}} {{pending.requestingSiteEngineer.lastName}} </td>
+            <td id = "plantPriceWE2" class="has-text-center"> {{pending.rentalCost.total}}</td>
+            <td><a v-on:click="accept" class="button is-success is-outlined">Accept</a> </td>
+            <td> <a v-on:click="reject" @click="isActive = !isActive" class="button is-danger is-outlined">Reject</a> </td>
+        </tr>
     </tbody>
 </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'App'
+  name: 'App',
+
+  data: function(){
+      return{
+        allrequest: [],
+        rejectedRequest:{},
+      }
+  },
+   mounted:function(){
+    this.pendingpurchaseOrder();
+  },
+  methods: {
+      pendingpurchaseOrder: function(){
+         axios.get("http://localhost:8080/api/requests?status=PENDING")
+        .then(response => {
+          this.allrequest = response.data._embedded.plantHireRequestDTOList;
+          console.log("Response", this.allrequest);
+        });
+      },
+      accept: function(inputOrder){
+           let i= document.getElementById("name").innerHTML
+           let params = i + "/accept"
+           console.log("Accept", i);
+        axios.get("http://localhost:8080/api/requests/", params)
+        .then(response => {
+           this.$snackbar.open("You have accepted this purchase order.");
+         console.log("Accept", response.data._embedded);
+        });
+      },
+      reject: function(inputOrder){
+           let i= document.getElementById("name").innerHTML
+           let params = i + "/reject"
+           console.log("Reject", i);
+        axios.get("http://localhost:8080/api/requests/", params)
+        .then(response => {
+          this.$snackbar.open("You have rejected this purchase order.");
+          console.log("Reject", response.data._embedded.plantHireRequestDTOList);
+          this.rejectedRequest = response.data._embedded.plantHireRequestDTOList;
+        });
+      },
+  }
 }
 </script>
 
