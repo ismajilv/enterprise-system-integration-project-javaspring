815549975b6d29e30232c918697ad2d5a2a0a656
Ardi Aasmaa <aasmaa.ardi@gmail.com>
2018-05-26 15:33:23 +0300

    .

--- project/BuildIT/back-end/src/main/java/com/buildit/procurement/domain/enums/RentItPurchaseOrderStatus.java
+++ project/BuildIT/back-end/src/main/java/com/buildit/procurement/domain/enums/RentItPurchaseOrderStatus.java
@@ -2,18 +2,31 @@ package com.buildit.procurement.domain.enums;
 
 public enum RentItPurchaseOrderStatus {
 
-	PENDING, REJECTED, OPEN, CLOSED, PENDING_EXTENSION;
+	PENDING, REJECTED, OPEN, CLOSED, PENDING_EXTENSION, CANCELLED, DISPATCHED
+	,RETURNED, REJECTED_BY_CUSTOMER, DELIVERED;
+
 
 	public PHRStatus convertToPHRStatus() {
 		switch (this) {
 			case PENDING:
 				return PHRStatus.PENDING_WORKS_ENGINEER_APPROVAL;
+			case DISPATCHED:
+			case DELIVERED:
+				return PHRStatus.PLANT_DELIVERED;
+			case REJECTED_BY_CUSTOMER:
+				return PHRStatus.CANCELLED;
+			case RETURNED:
+				return PHRStatus.PAID;
+			case CANCELLED:
+				return PHRStatus.CANCELLED;
 			case OPEN:
+				return  PHRStatus.ACCEPTED_BY_RENTAL_PARTNER;
 			case CLOSED:
 			case PENDING_EXTENSION:
 				return PHRStatus.PENDING_RENTAL_PARTNER_APPROVAL;
 			case REJECTED:
 				return PHRStatus.REJECTED;
+
 			default:
 				throw new IllegalArgumentException("Unknown PO status: " + this);
 		}
