82f25e713259a53854c96c01be6735f4737528cf
Polad Mahmudov <polad.mahmudov@tudeng.ut.ee>
2018-05-27 21:08:44 +0300

    Created user authentication in front-end

--- project/BuildIT/front-end/BuildITFrontend/src/router/index.js
+++ project/BuildIT/front-end/BuildITFrontend/src/router/index.js
@@ -2,20 +2,42 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import SiteEngineer from '../views/SiteEngineer.vue';
 import AllEngineers from '../views/AllEngineers.vue';
+import Login from '../components/Login.vue';
+import auth from '../auth';
 
 Vue.use(Router);
+
+function requireAuth (to, from, next) {
+  if (!auth.loggedIn()) {
+    next({
+      path: '/login',
+      query: { redirect: to.fullPath }
+    })
+  } else {
+    next()
+  }
+}
 
 export default new Router({
   routes: [
     {
       path: '/SiteEngineer',
       name: 'SiteEngineer',
-      component: SiteEngineer
+      component: SiteEngineer,
+      beforeEnter: requireAuth
     },
     {
       path: '/',
       name: 'AllEngineers',
-      component: AllEngineers
+      component: AllEngineers,
+      beforeEnter: requireAuth
+    },
+    { path: '/login', component: Login },
+    { path: '/logout',
+      beforeEnter (to, from, next) {
+        auth.logout();
+        next('/')
+      }
     }
   ]
 })
