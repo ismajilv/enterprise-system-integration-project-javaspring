b9b5835a6c3879c6af6dabb1446de6ff6b171a42
John Doe <kadri.oluwagbemi@gmail.com>
2018-05-27 09:16:48 +0300

    RentIT update

--- project/RentIT/front-end/RentITfront/src/components/PurchaseOrderApproval.vue
+++ project/RentIT/front-end/RentITfront/src/components/PurchaseOrderApproval.vue
@@ -5,42 +5,33 @@
     <table class="table is-table-bordered is-table-striped is-fullwidth">
     <thead>
         <tr>
-            <th class="has-text-center">id</th>
             <th class="has-text-center">Name</th>
             <th class="has-text-center">Start Date</th>
             <th class="has-text-center">End Date</th>
-            <th class="has-text-center">Site Engineer</th>
             <th class="has-text-center">Price</th>
+            <th class="has-text-center">Status</th>
             <th class="has-text-center">Actions</th>
             <th class="has-text-center">Actions</th>
-
         </tr>
     </thead>
     <tbody>
           <tr class="table-row-rentit" v-for="(pending, index) in allrequest" :key="pending._id" >
-            <td class="has-text-center" id="name"> {{pending._id}}</td>
+            <td v-show="seen" class="has-text-center" id="app"> {{pending._id}}</td>
             <td id = "plantNameWE2" class="has-text-center">{{pending.plant.name}}</td>
             <td id = "plantStartDateWE2" class="has-text-center">{{pending.rentalPeriod.startDate}}</td>
             <td id = "plantEndDateWE2" class="has-text-center">{{pending.rentalPeriod.endDate}}</td>
-            <td class="has-text-center"></td>
-             <transition name="fade">
-            <td v-if="show" id = "plantPriceWE2" class="has-text-center">{{pending.plant.price}}</td>
-             </transition>
+            <td id = "plantPriceWE2" class="has-text-center">{{pending.plant.price}}</td>
+            <td id = "plantPriceWE2" class="has-text-center">{{pending.status}}</td>
             <td><a v-on:click="accept(index)" class="button is-success is-outlined">Accept</a> </td>
             <td><a v-on:click="reject(index)" class="button is-danger is-outlined">Reject</a> </td>
-             <td><a v-on:click="orderDetails(index)" class="button is-danger is-primary">Order details</a> </td>
         </tr>
     </tbody>
-    <h2> View More </h2>
-    <ul v-for="a in acceptStatus" :key="a._id">
-    <li>Rental period  {{a.rentalPeriod}} </li>
-    <li>View price  {{a.rentalCost}} </li>
-      </ul>
 </table>
   </div>
 </template>
 
 <script>
+
 import axios from 'axios';
 
 export default {
@@ -50,8 +41,9 @@ export default {
       return{
         allrequest: [],
         rejectedRequest:{},
+        orderInfo: {},
         acceptStatus: {},
-        show: false
+        seen: false
       }
   },
    mounted:function(){
@@ -65,19 +57,22 @@ export default {
           console.log("Response", this.allrequest);
         });
       },
-      orderDetails: function(inputOrder){
-          let orderInfo = this.acceptStatus;
-           console.log("View Order Information", orderInfo);
-      },
+      // orderDetails: function(inputOrder){
+      //     var orderInfo;
+      //     this.orderInfo = this.acceptStatus;
+      //      console.log("View Order Information", orderInfo);
+      //      return orderInfo;
+      // },
       accept: function(inputOrder){
            let i= this.allrequest[inputOrder]._id;
            let params = i + "/accept"
            console.log("Accept", i);
         axios.get("http://localhost:8080/api/requests/", params)
         .then(response => {
-           this.$snackbar.open("You have accepted this purchase order.");
-           this.acceptStatus = response.data._embedded.plantHireRequestDTOList;
-           console.log("Accept Status", acceptStatus);
+           this.$snackbar.open("You have accepted this plant request.");
+           var acceptStatus;
+           acceptStatus = response.data._embedded;
+           console.log("Accept Status", acceptStatus)
            return acceptStatus;
         });
       },
@@ -87,9 +82,7 @@ export default {
            console.log("Reject", i);
         axios.get("http://localhost:8080/api/requests/", params)
         .then(response => {
-          this.$snackbar.open("You have rejected this purchase order.");
-          console.log("Reject", response);
-          this.rejectedRequest = response.data._embedded.plantHireRequestDTOList;
+          this.$snackbar.open("You have rejected this plant request.");
         });
       },
   }
