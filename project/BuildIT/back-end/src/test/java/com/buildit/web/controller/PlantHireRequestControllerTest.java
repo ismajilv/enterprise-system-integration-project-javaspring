8934b45a090d93febfeadd14b3cda29eed7e39ef
Tural Ismayilov <tuisma@ttu.ee>
2018-05-28 01:50:58 +0300

    Update to plants-dataset.sql and Unit test for CC3 is created

--- project/BuildIT/back-end/src/test/java/com/buildit/web/controller/PlantHireRequestControllerTest.java
+++ project/BuildIT/back-end/src/test/java/com/buildit/web/controller/PlantHireRequestControllerTest.java
@@ -90,7 +90,7 @@ public class PlantHireRequestControllerTest {
         PlantHireRequestDTO plantHireRequestDTO = mapper.readValue(result.getResponse().getContentAsString(), PlantHireRequestDTO.class);
 
         //Some of the details are checked
-        assertThat(plantHireRequestDTO.get_id()).isEqualTo(2);
+        assertThat(plantHireRequestDTO.get_id()).isEqualTo(14);
         assertThat(plantHireRequestDTO.getRentalCost()).isEqualTo("400.00");
         assertThat(plantHireRequestDTO.getRentalPeriod().getStartDate()).isEqualTo(LocalDate.now().plusDays(1));
         assertThat(plantHireRequestDTO.getRentalPeriod().getEndDate()).isEqualTo(LocalDate.now().plusDays(3));
@@ -138,6 +138,8 @@ public class PlantHireRequestControllerTest {
         MvcResult result = mockMvc.perform(post("/api/requests")
                 .content(mapper.writeValueAsString(dto))
                 .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", isEmptyOrNullString()))
                 .andReturn();
 
         PlantHireRequestDTO dto2 = mapper.readValue(result.getResponse().getContentAsString(), PlantHireRequestDTO.class);
@@ -145,10 +147,18 @@ public class PlantHireRequestControllerTest {
         MvcResult result2 = mockMvc.perform(post("/api/requests/" + dto2.get_id() + "/cancel")
                 .content(mapper.writeValueAsString(dto2))
                 .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Location", isEmptyOrNullString()))
                 .andReturn();
 
         dto2 = mapper.readValue(result2.getResponse().getContentAsString(), PlantHireRequestDTO.class);
 
         assertThat(dto2.getStatus()).isEqualTo(PHRStatus.CANCELLED);
+    }
+
+    @Test
+    @Sql("/plants-dataset.sql")
+    public void cancelPlantHireRequestTest_CC4() throws Exception {
+
     }
 }
