6771d047ba0fdd64cb932ddbaac989e88daede0a
Polad Mahmudov <polad.mahmudov@tudeng.ut.ee>
2018-05-28 10:42:30 +0300

    Modified PHR creation (fixes), deleted supplier selection

--- project/BuildIT/front-end/BuildITFrontend/src/components/PlantsRequest/QueryResult.vue
+++ project/BuildIT/front-end/BuildITFrontend/src/components/PlantsRequest/QueryResult.vue
@@ -3,19 +3,17 @@
     <thead>
         <tr>
             <th class="has-text-center">Name</th>
-            <th class="has-text-center">Description</th>
             <th class="has-text-center">Price</th>
-            <th class="has-text-center">Actions</th>
+            <th class="has-text-center">Action</th>
         </tr>
     </thead>
     <tbody>
         <tr v-for="plant in plants" :key="plant.id" class="table-row">
-            <td>{{ plant.name }}</td>
-            <td>{{ plant.description }}</td>
-            <td class="has-text-right">{{ plant.price }}</td>
-            <td>
-                <a class="button is-link is-small is-outlined"
-                @click="submit(plant)">Select plant</a>
+            <td class="has-text-center">{{ plant.name }}</td>
+            <td class="has-text-center">{{ plant.pricePerDay }}</td>
+            <td class="has-text-center">
+                <button class="button is-link is-small is-outlined"
+                @click="submit(plant)">Select plant</button>
             </td>
         </tr>
     </tbody>
